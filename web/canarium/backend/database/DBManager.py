adf10fca6ae642bc6204c12348384937e45996e8
Chrisando Ryan <chrisando.siahaan@gmail.com>
2023-10-25 09:15:59 +0700

    update canarium

--- web/canarium/backend/database/DBManager.py
+++ web/canarium/backend/database/DBManager.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 class DBManager:
-    def __init__(self, database='canarium', host="db", user="root", password_file=None):
+    def __init__(self, database='example', host="db", user="root", password_file=None):
         pf = open(password_file, 'r')
         self.connection = mysql.connector.connect(
             user=user, 
